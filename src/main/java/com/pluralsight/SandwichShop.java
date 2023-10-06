cc36f8d99479756140445db61a6284b9b5ed33ac
rickeyglover <rickeygloverw@gmail.com>
2023-10-06 11:23:01 -0400

    corrected if and else statements

--- src/main/java/com/pluralsight/SandwichShop.java
+++ src/main/java/com/pluralsight/SandwichShop.java
@@ -29,13 +29,13 @@ public class SandwichShop {
         if (age <= 17 && size.equals("Regular")){price1sd = price1 - (price1 * .10);
         System.out.printf("price after student discount: " + "%.2f", price1sd);}
 
-        if (age >= 65 && size.equals("Regular")){price1ed = price1 - (price1 * .20);
+        else if (age >= 65 && size.equals("Regular")){price1ed = price1 - (price1 * .20);
         System.out.printf("price after senior discount: " + "%.2f", price2ed);}
 
         if (age <= 17 && size.equals("Large")){price2sd = price2 - (price2 * .10);
         System.out.printf("price after student discount: " + "%.2f", price2sd);}
 
-        if (age >= 65 && size.equals("Large")){price2ed = price2 - (price2 * .20);
+        else if (age >= 65 && size.equals("Large")){price2ed = price2 - (price2 * .20);
         System.out.printf("price after senior discount: " + "%.2f", price2ed);}
 
 
